9c23d4c6c863d060d9ca7ba9fc24853f7bab3562
YuneeeM <dana512@naver.com>
2023-01-03 00:26:51 +0900

    develop branch 추가

--- android/app/src/main/java/com/umc/badjang/MainActivity.kt
+++ android/app/src/main/java/com/umc/badjang/MainActivity.kt
@@ -2,7 +2,7 @@ package com.umc.badjang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//develop 브랜치 추가
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
