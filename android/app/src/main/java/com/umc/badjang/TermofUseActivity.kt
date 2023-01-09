7371e2e2872883f4bdfd09d1dff998c7ef9c151c
YuneeeM <dana512@naver.com>
2023-01-09 18:48:10 +0900

    issue #02 feature: 로그인 회원가입 레이아웃 완성

---
+++ android/app/src/main/java/com/umc/badjang/TermofUseActivity.kt
@@ -0,0 +1,11 @@
+package com.umc.badjang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TermofUseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_termofuse)
+    }
+}
---
+++ android/app/src/main/java/com/umc/badjang/VerificationIDActivity.kt
@@ -0,0 +1,11 @@
+package com.umc.badjang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerificationIDActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.verification_id_dialog)
+    }
+}
