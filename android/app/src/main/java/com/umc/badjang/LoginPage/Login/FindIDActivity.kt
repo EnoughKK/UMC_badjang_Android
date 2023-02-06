f5e31e3f975f7a20777564cd4579b31c58ad3f1a
YuneeeM <dana512@naver.com>
2023-02-06 20:30:09 +0900

    issue #21 feature : 전화번호 인증 구현

--- android/app/src/main/java/com/umc/badjang/LoginPage/Login/FindIDActivity.kt
+++ android/app/src/main/java/com/umc/badjang/LoginPage/Login/FindIDActivity.kt
@@ -6,11 +6,21 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
 import com.umc.badjang.LoginPage.LoginActivity
 import com.umc.badjang.R
 import com.umc.badjang.databinding.ActivityFindIdactivityBinding
 import com.umc.badjang.utils.Constants.TAG
+import java.util.concurrent.TimeUnit
+
+val auth = Firebase.auth
+var verificatioId=""
 
 class FindIDActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFindIdactivityBinding// viewBinding
@@ -25,6 +35,24 @@ class FindIDActivity : AppCompatActivity() {
         binding.FindIDUpBtn.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
+
+        val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) { }
+            override fun onVerificationFailed(e: FirebaseException) {
+            }
+            override fun onCodeSent(verificationId: String, token: PhoneAuthProvider.ForceResendingToken) {
+                this@FindIDActivity.verificationId = verificationId
+            }
+        }
+
+        val optionsCompat =  PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber("+821012345678")
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(callbacks)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(optionsCompat)
+        auth.setLanguageCode("kr")
 
         binding.FindIDAuthBtn.setOnClickListener {
             FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
@@ -47,5 +75,34 @@ class FindIDActivity : AppCompatActivity() {
         binding.FindIDPWPage.setOnClickListener {
             startActivity(Intent(this, FindPWActivity::class.java))
         }
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    //인증성공
+                }
+                else {
+                    //인증실패
+                }
+            }
+    }
+
+    private fun phoneNumber82(msg : String) : String{
+        val firstNumber : String = msg.substring(0,3)
+        var phoneEdit = msg.substring(3)
+
+        when(firstNumber){
+            "010" -> phoneEdit = "+8210$phoneEdit"
+            "011" -> phoneEdit = "+8211$phoneEdit"
+            "016" -> phoneEdit = "+8216$phoneEdit"
+            "017" -> phoneEdit = "+8217$phoneEdit"
+            "018" -> phoneEdit = "+8218$phoneEdit"
+            "019" -> phoneEdit = "+8219$phoneEdit"
+            "106" -> phoneEdit = "+82106$phoneEdit"
+        }
+        Log.d("국가코드로 변경된 번호 ",phoneEdit)
+        return phoneEdit
     }
 }
